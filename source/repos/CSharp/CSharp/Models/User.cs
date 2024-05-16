5806af6d5918f94800d96f2e511049a37d4c98e7
constpereiradev <amandapereiradevcontact@gmail.com>
2024-05-16 15:02:33 -0300

    primeiro commit

---
+++ source/repos/CSharp/CSharp/Models/User.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSharp.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+        
+    }
+}
