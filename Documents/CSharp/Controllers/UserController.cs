e7b593c616fab5bcf6b37330a0a002f70d6d5477
constpereiradev <amandapereiradevcontact@gmail.com>
2024-05-20 17:29:18 -0300

    projetos

---
+++ Documents/CSharp/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+namespace CSharp.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly ILogger<UserController> _logger;
+
+        public UserController(ILogger<UserController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Profile()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
